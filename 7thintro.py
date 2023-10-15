adea3cc93f327e0a462c73be89e1beeff6f51cc3
Triantafyllos Xydis <triantafyllosxyd@gmail.com>
2023-10-15 13:23:22 +0300

    change file names

--- 7thintro.py
+++
@@ -1,9 +0,0 @@
-stringtext = "label"
-
-new_string = ""
-for c in stringtext:
-    temp = ord(c)
-    xor_temp = temp ^ 13
-    new_char = chr(xor_temp)
-    new_string += new_char
-print(new_string)
