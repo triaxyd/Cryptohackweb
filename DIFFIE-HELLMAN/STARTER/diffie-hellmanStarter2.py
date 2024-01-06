03fc7036653bcc48900324420f99db5f40b30c57
Triantafyllos Xydis <triantafyllosxyd@gmail.com>
2024-01-06 11:05:36 +0200

    some changes

--- DIFFIE-HELLMAN/STARTER/diffie-hellmanStarter2.py
+++ DIFFIE-HELLMAN/STARTER/diffie-hellmanStarter2.py
@@ -1,3 +1,17 @@
+#https://math.stackexchange.com/questions/124408/finding-a-primitive-root-of-a-prime-number
+from sympy import primefactors
 p = 28151
+phip = p-1
+prime_factors = primefactors(phip)
+powers_to_test = list(int(phip/pf) for pf in prime_factors)
 
-
+# test for range 1-100 to see
+for i in range(1, 101):
+    is_primitive = True
+    for pf in powers_to_test:
+        if pow(i, pf, p) == 1:
+            is_primitive = False
+            break 
+    if is_primitive:
+        print(f'Found smallest primitive root: {i}')
+        break 
